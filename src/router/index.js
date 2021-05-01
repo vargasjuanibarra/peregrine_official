4c0435e908737da22dd7779ad3763cfee9135677
vargasjuanibarra <78344342+vargasjuanibarra@users.noreply.github.com>
2021-05-01 17:58:43 +0800

    changes removed 404 route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -23,11 +22,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "store" */ '../views/Store.vue')
-  },
-  {
-    path: '*',
-    name: 'NotFound',
-    component: NotFound
   },
   
 ]

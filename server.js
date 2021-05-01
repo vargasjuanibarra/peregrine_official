58d7884fa9f9a02b7e43f5117f2926ebdc236b81
vargasjuanibarra <78344342+vargasjuanibarra@users.noreply.github.com>
2021-05-01 17:41:38 +0800

    revert

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const port = process.env.PORT || 8080;
-const path = require('path')
 const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, "/dist/")))
+app.use(express.static(__dirname + "/dist/"))
 app.get(/.*/, function(req, res) {
-    res.sendFile(path.join(__dirname, "/dist/index.html"))
+    res.sendfile.apply(__dirname + "/dist/index.html")
 });
 app.listen(port);
 
